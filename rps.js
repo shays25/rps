5ac53a4ce8112d5e678cfee6c55b4d0f78e1bb57
Shanarra <shanarrasapida@gmail.com>
2023-02-07 12:37:08 -0800

    add if else statements to playRound

--- rps.js
+++ rps.js
@@ -9,22 +9,43 @@ const computerSelection = getComputerChoice(choices);
 
 console.log("The computer chooses " + computerSelection)
 
-let playerSelection = "rock"
+let playerSelection = prompt("Select: rock, paper, or scissors?")
 
-console.log("The player chooses " + playerSelection)
 function playRound(playerSelection, computerSelection) {
    
-    if (computerSelection === "rock")
+    if (playerSelection === "rock" && computerSelection === "rock")
     return 'It is a tie!';
 
-        else if(computerSelection === "paper")
-        return 'Player loses!';
+        else if(playerSelection === "rock" && computerSelection === "paper")
+        return 'Paper beats rock, you lose!';
 
-        else (computerSelection === "scissors")
-        return 'Player wins!';
-    
+        else if (playerSelection === "rock" && computerSelection === "scissors")
+        return 'Rock beats scissors, you win!';
+
+        else if (playerSelection === "paper" && computerSelection === "rock")
+        return 'Paper beats rock, you win!';
+
+        else if (playerSelection === "paper" && computerSelection === "paper")
+        return 'It is a tie!';
+
+        else if (playerSelection === "paper" && computerSelection === "scissors")
+        return 'Scissors beat paper, you lose!';
+
+        else if (playerSelection === "scissors" && computerSelection === "rock")
+        return 'Rock beats scissors, you lose!';
+
+        else if (playerSelection === "scissors" && computerSelection === "paper")
+        return 'Scissors beat paper, you win!';
+
+        else if (playerSelection === "scissors" && computerSelection === "scissors")
+        return 'It is a tie!';
 }
 
+function game (playRound) {
+    for (let i = 0; i < 5; i++) {
+
+    }
+}
 console.log(playRound(playerSelection, computerSelection));
 
 
