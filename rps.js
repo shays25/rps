3a6d3464480a3b81698f364c548c872b6d45d16f
Shanarra <shanarrasapida@gmail.com>
2023-02-09 19:33:48 -0800

    add for loop and scores

--- rps.js
+++ rps.js
@@ -1,55 +1,75 @@
 
 
-const choices = ['rock', 'paper', 'scissors']
+ let playerScore = 0;
+ let computerScore = 0;
+ let roundresult = 0;
+ let rock = "rock";
+ let paper = "paper";
+ let scissors = "scissors"
+game(); 
+
+function game () { 
+    const choices = [rock, paper, scissors]
+   
+    for (let i = 0; i < 5; i++) {
+        let userPrompt = prompt("Enter: rock, paper, or scissors?");
+        let playerSelection = userPrompt.toLowerCase();
+        const computerSelection = getComputerChoice(choices);
+        alert ("The computer chose " + computerSelection)
+        alert(playRound(playerSelection, computerSelection));
+        if (playerScore >= 3) {
+            alert ("You won the game!")
+            break;
+        }
+    
+        if (computerScore >= 3) {
+            alert ("The computer won the game!")
+            break;
+        }  
+    }
+    alert("Your score is " + playerScore);
+    alert("The computer's score is " + computerScore)
+}
+
+//array for computer's randomized choice
+
 function getComputerChoice (choices) {
     return choices [Math.floor(Math.random()*choices.length)];
 }
 
-const computerSelection = getComputerChoice(choices);
-
-console.log("The computer chooses " + computerSelection)
-
-let playerSelection = prompt("Select: rock, paper, or scissors?")
-
-alert ("The computer chose " + computerSelection)
+//single round of the game
 function playRound(playerSelection, computerSelection) {
    
-    if (playerSelection === "rock" && computerSelection === "rock")
+    if (playerSelection === computerSelection)
     return 'It is a tie!';
 
-        else if(playerSelection === "rock" && computerSelection === "paper")
+        else if(playerSelection === rock && computerSelection === paper) {
+        computerScore = computerScore + 1;
         return 'Paper beats rock, you lose!';
+        }
 
-        else if (playerSelection === "rock" && computerSelection === "scissors")
+        else if (playerSelection === rock && computerSelection === scissors) {
+        playerScore = playerScore + 1;
         return 'Rock beats scissors, you win!';
+        }
 
-        else if (playerSelection === "paper" && computerSelection === "rock")
+        else if (playerSelection === paper && computerSelection === rock) {
+        playerScore = playerScore + 1;
         return 'Paper beats rock, you win!';
+        }
 
-        else if (playerSelection === "paper" && computerSelection === "paper")
-        return 'It is a tie!';
-
-        else if (playerSelection === "paper" && computerSelection === "scissors")
+        else if (playerSelection === paper && computerSelection === scissors) {
+        computerScore = computerScore + 1;
         return 'Scissors beat paper, you lose!';
+        }
 
-        else if (playerSelection === "scissors" && computerSelection === "rock")
+        else if (playerSelection === scissors && computerSelection === rock) {
+        computerScore = computerScore + 1;    
         return 'Rock beats scissors, you lose!';
-
-        else if (playerSelection === "scissors" && computerSelection === "paper")
+        }
+    
+        else if (playerSelection === scissors && computerSelection === paper) {
+        playerScore = playerScore + 1; 
         return 'Scissors beat paper, you win!';
-
-        else if (playerSelection === "scissors" && computerSelection === "scissors")
-        return 'It is a tie!';
-}
-
-alert (playRound(playerSelection, computerSelection));
-
-function game (playRound) {
-    for (let i = 0; i < 5; i++) {
-
+        }
     }
-}
-
-
-
-//const playerSelection = "rock";
