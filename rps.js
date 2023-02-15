4c2e4ab4c9702154fff1af094d8012f36a5901a5
Shanarra <shanarrasapida@gmail.com>
2023-02-14 19:40:35 -0800

    add if else statements in while loop to validate user input

--- rps.js
+++ rps.js
@@ -10,44 +10,55 @@
  //invoke the game function
  game(); 
 
-//function that will continue to play until player or computer gets 5 points
+//function that will allow game to play until player or computer gets 5 points
 function game () { 
     const choices = [rock, paper, scissors]
    
     while (playerScore <5 || computerScore <5 ) {
-        let userPrompt = prompt("Enter: rock, paper, or scissors?");
-        let playerSelection = userPrompt.toLowerCase();
+        let playerSelection = prompt("Enter: rock, paper, or scissors?").toLowerCase();
+        if (playerSelection === rock || playerSelection === paper || playerSelection === scissors) {
+        }
+            //continue statements jumps over one iteration in a loop
+            else if (playerSelection === "" || playerSelection === null) {
+                alert ("Error! Please start again");
+                continue; 
+             }
+             
+             else {
+                alert ("Error! Please start again");
+                continue;
+             }
+
         const computerSelection = getComputerChoice(choices);
-        alert ("The computer chose " + computerSelection)
+        alert("The computer chose " + computerSelection);
         alert(playRound(playerSelection, computerSelection));
-        //why does roundResult need to be inside? Why can't it be a global variable? Because it'll only be used in this loop?
         let roundResult = "Your score: " + playerScore + "\nThe computer's score: " + computerScore;
         alert(roundResult);
+
+         //break statements jump out of a loop
         if (playerScore >= 5) {
             alert ("You won the game!")
             break;
         }
-    
+
         if (computerScore >= 5) {
             alert ("The computer won the game!")
             break;
-        }  
+        }
     }
-
 }
 
 //array for computer's randomized choice
-
 function getComputerChoice (choices) {
     return choices [Math.floor(Math.random()*choices.length)];
 }
 
 //single round of the game
 function playRound(playerSelection, computerSelection) {
-   
-    if (playerSelection === computerSelection) {
-    return 'It is a tie!';
-    }
+        
+        if (playerSelection === computerSelection) {
+        return 'It is a tie!';
+        }
 
         else if (playerSelection === rock && computerSelection === paper) {
         computerScore = computerScore + 1;
@@ -78,11 +89,4 @@ function playRound(playerSelection, computerSelection) {
         playerScore = playerScore + 1; 
         return 'Scissors beat paper, you win!';
         }
-
     }
-
-
-     //else (playerSelection === "") {
-        //alert ('Error');
-        //alert (userPrompt);
-        //}
